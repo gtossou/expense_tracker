426cd55cd7e31b47fe48fb4bd6465e73561c24fa
gtossou <gil.tososu@gmail.com>
2020-03-25 09:22:58 +0100

    Added front end

--- src/App.js
+++ src/App.js
@@ -6,19 +6,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is my update.
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
+      <div className="new-item-zone">
+        
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const touse = document.querySelector(".touse")
+
+touse.addEventListener("click",function(){
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+    <React.StrictMode>
+    {/* <App /> */}
   </React.StrictMode>,
   document.getElementById('root')
+  )}
+  
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
